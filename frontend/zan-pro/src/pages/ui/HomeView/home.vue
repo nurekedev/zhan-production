c0a3f5df38f206acb626d82b78e9202bda091f3b
holyPaladu <123793938+holyPaladu@users.noreply.github.com>
2023-11-25 15:18:23 +0600

    main page

--- frontend/zan-pro/src/pages/ui/HomeView/home.vue
+++ frontend/zan-pro/src/pages/ui/HomeView/home.vue
@@ -2,10 +2,56 @@
 </script>
 
 <template>
-    <main>
+    <main class="home-main">
         <section>
-            <span></span>
+            <img src="../../model/main.jpeg" />
+            <div class="img-t-q">
+                <article>
+                    <h1>Работа в Польше</h1>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore 
+                        magna aliqua. Auctor elit sed vulputate mi.
+                    </p>
+                </article>
+                <form method="post">
+                    <label>Уточните что вы хотите?</label>
+                    <input type="text" placeholder="Имя">
+                    <input type="tel" pattern="[8]{1}-[0-9]{3}-[0-9]{3}-[0-9]{2}-[0-9]{2}" placeholder="Телефон">
+                    <button type="submit">Отправить</button>
+                </form>
+            </div>
         </section>
+        <article>
+            <div class="about">
+                <h1>О компании</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                    sed do eiusmod tempor incididunt ut labore et dolore magna
+                    aliqua. Auctor elit sed vulputate mi. Faucibus scelerisque
+                    eleifend donec pretium vulputate sapien nec sagittis aliquam.
+                    At tempor commodo ullamcorper a lacus vestibulum sed arcu non.
+                    Amet purus gravida quis blandit. Scelerisque purus semper eget
+                    duis at tellus at urna condimentum. Et malesuada fames ac turpis egestas maecenas. 
+                </p>
+            </div>
+            <aside>
+                <div class="review-title">
+                    <h1>Отзывы клиентов</h1>
+                    <img class="review-img">
+                    <h2>Динара Асылхановна</h2>
+                    <p>
+                        "Share a real testimonial that hits some of your benefits (but isn’t too sales-y)."
+                    </p>
+                    <div class="home-pagination">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+            </aside>
+        </article>
     </main>
 </template>
 
