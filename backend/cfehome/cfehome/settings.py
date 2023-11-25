c37b9ddf7b2582aa4363f04b31768d3fcf7e64c8
Nurzhan <snurekeee03@gmail.com>
2023-11-25 21:30:03 +0600

    test

--- backend/cfehome/cfehome/settings.py
+++ backend/cfehome/cfehome/settings.py
@@ -218,3 +218,12 @@ SPECTACULAR_SETTINGS = {
     "DESCRIPTION": "DRF API Endpoints of website",
     "VERSION": "1.0.0",
 }
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'snurekeee03@gmail.com'
+EMAIL_HOST_PASSWORD = 'bvpqshybjtztzugg'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
