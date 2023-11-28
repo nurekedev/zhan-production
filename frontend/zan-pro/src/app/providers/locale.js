63b28d03e2767e7e48da238b82be508faa5948d3
meaculpv <olzhabaevbatyr@gmail.com>
2023-11-28 16:23:40 +0600

    Fixed imports

--- frontend/zan-pro/src/app/providers/locale.js
+++ frontend/zan-pro/src/app/providers/locale.js
@@ -3,14 +3,12 @@ import ru from './locale/ru.json';
 import en from './locale/en.json';
 import kz from './locale/kz.json';
 import pl from './locale/pl.json';
-import store from '../../shared/store/index.js';
 
-console.log(store.getters.getCurrentLocale);
-const locales = store.getters.getLocales;
+
 
 const i18n = createI18n({
-    locale: store.getters.getCurrentLocale,
-    fallbackLocale: 'ru',
+    locale: 'en',
+    fallbackLocale: 'en',
     messages: {
         ru: ru,
         en: en,
