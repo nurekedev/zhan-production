d4fc81bcd14da0517f63b5ef1ae7fa7d1cda5b28
meaculpv <olzhabaevbatyr@gmail.com>
2023-11-30 17:34:44 +0600

    Implement toast notification component

---
+++ frontend/zan-pro/src/shared/ToastNotificationComponent/toastNotificationComponent.vue
@@ -0,0 +1,25 @@
+<script>
+    export default {
+        name: 'ToastNotification',
+        props: {message: ''},
+        data() {
+            return {
+                visible: false,
+            }
+        },
+        methods: {
+            showToast() {
+                this.visible = true;
+                setTimeout(() => {
+                    this.visible = false
+                }, 3000);
+            }
+        }
+    }
+</script>
+
+<template>
+    <div class="toast-container" v-if="visible">
+        <div class="toast">{{ message }}</div>
+    </div>
+</template>
