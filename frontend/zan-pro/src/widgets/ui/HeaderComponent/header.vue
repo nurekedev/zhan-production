97af97682c9b506a42443482c6a53701bb3a129e
meaculpv <olzhabaevbatyr@gmail.com>
2023-12-04 19:54:05 +0600

    Fix visual on Firefox && Add navbar animation

--- frontend/zan-pro/src/widgets/ui/HeaderComponent/header.vue
+++ frontend/zan-pro/src/widgets/ui/HeaderComponent/header.vue
@@ -11,17 +11,22 @@
         </router-link>
         <nav>
             <ul>
-                <!-- <router-link to="/employment"><li>Трудоустройство в Польше</li></router-link> -->
-                <!-- <router-link to="/contact"><li>Контакты</li></router-link> -->
-                <router-link to="/employment"><li>{{ $t('navWork') }}</li></router-link>
-                <router-link to="/contact"><li>{{ $t('navContact') }}</li></router-link>
+                <router-link to="/"><li><p>{{ $t('navHome') }}</p></li></router-link>
+                <router-link to="/employment"><li><p>{{ $t('navWork') }}</p></li></router-link>
+                <router-link to="/contact"><li><p>{{ $t('navContact') }}</p></li></router-link>
             </ul>
         </nav>
-        <div class="burger-btn">
-            <svg xmlns="http://www.w3.org/2000/svg" width="32" height="8" viewBox="0 0 32 8" fill="none">
-                <rect width="32" height="2" fill="black" />
-                <rect y="6" width="32" height="2" fill="black" />
-            </svg>
+        <div class="burger-container">
+            <label class="burger-menu">
+                <input type="checkbox">
+            </label>
+            <nav class="nav-links">
+                <ul>
+                    <router-link to="/"><li>{{ $t('navHome') }}</li></router-link>
+                    <router-link to="/employment"><li>{{ $t('navWork') }}</li></router-link>
+                    <router-link to="/contact"><li>{{ $t('navContact') }}</li></router-link>
+                </ul>
+            </nav>
         </div>
     </header>
 </template>
