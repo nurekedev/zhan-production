64344342ab173ae7cc5a553c26827dda14492b19
Nurzhan <snurekeee03@gmail.com>
2023-12-15 00:10:47 +0600

    Fixing 415 ERROR

--- backend/cfehome/cfehome/settings.py
+++ backend/cfehome/cfehome/settings.py
@@ -143,12 +143,31 @@ CORS_ORIGIN_WHITELIST = [
 
 CORS_ALLOW_CREDENTIALS = True
 
+CORS_ALLOW_HEADERS = [
+'accept',
+'accept-encoding',
+'authorization',
+'content-type',
+'dnt',
+'origin',
+'user-agent',
+'x-csrftoken',
+'x-requested-with',
+]
 
-
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
 
 CSRF_TRUSTED_ORIGINS = [
     'http://localhost:5173',
 ]
+
 
 CSRF_COOKIE_SECURE = True
 
